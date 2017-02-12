01130c54f8d0dd6b8bbeadff7205a741daf8e8a3
RyanCPeters <petersryan84@gmail.com>
2017-02-12 14:52:38 -0800

    Initial push with only the packaging namespace set up

---
+++ src/Model/Tile.java
@@ -0,0 +1,13 @@
+package Model;
+
+/** I think we should initially define this basic tile object as an abstract class then define 26 unique subclasses.
+ * One for each letter of the english alphabet.
+ *
+ * I don't know how we want to go about instantiating our tile objects yet, but after instantiation they will represent
+ * a predefined kit of states that the space objects will be able to pass to the controller when we need to update the
+ * view to graphically represent the current state of the board.
+ * @author Ryan Peters
+ * @date 2/12/2017
+ */
+public class Tile {
+}
---
+++ src/View/Board.java
@@ -0,0 +1,8 @@
+package View;
+
+/** This will extend the content pane or the scene, depending upon what library we decide to use in building the UI.
+ * @author Ryan Peters
+ * @date 2/12/2017
+ */
+public class Board {
+}
