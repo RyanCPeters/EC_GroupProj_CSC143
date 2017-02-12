62eda771772f956e8c46d99c114a7029d55e2f53
Ryan Peters <petersryan84@gmail.com>
2017-02-12 14:58:56 -0800

    Add files via upload
    Initial push of basic project structure and class naming

---
+++ Scribble/src/Model/ModelBase.java
@@ -0,0 +1,9 @@
+package Model;
+
+/**Our model base will be where we handle the instantiation and access rights with regards to the arrays of space and
+ * tile objects.
+ * @author Ryan Peters
+ * @date 2/12/2017
+ */
+public class ModelBase {
+}
---
+++ Scribble/src/Model/Space.java
@@ -0,0 +1,8 @@
+package Model;
+
+/**The space objects are where we describe the means to access and set the state of each tile space on the board.
+ * @author Ryan Peters
+ * @date 2/12/2017
+ */
+public class Space {
+}
