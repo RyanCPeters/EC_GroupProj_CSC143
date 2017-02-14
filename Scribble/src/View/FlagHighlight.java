8acf6697638477f1e66a2ee7d3db99b0d9ed4967
Ryan Peters <petersryan84@gmail.com>
2017-02-13 20:17:15 -0800

    Delete FlagHighlight.java

--- Scribble/src/View/FlagHighlight.java
+++
@@ -1,9 +0,0 @@
-package View;
-
-/** This enum is just an example of how we might use a public constant enum to flag our board's spaces as highlighted
- * when the user is selecting where to place a word on the board.
- * @author Ryan Peters
- * @date 2/12/2017
- */
-public enum FlagHighlight {
-}
