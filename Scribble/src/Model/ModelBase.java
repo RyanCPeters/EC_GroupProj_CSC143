37f3b4eb9a0e9a07529954d3fab0071a870971c6
Ryan Peters <petersryan84@gmail.com>
2017-02-13 20:16:46 -0800

    Delete ModelBase.java

--- Scribble/src/Model/ModelBase.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-/**Our model base will be where we handle the instantiation and access rights with regards to the arrays of space and
- * tile objects.
- * @author Ryan Peters
- * @date 2/12/2017
- */
-public class ModelBase {
-}
