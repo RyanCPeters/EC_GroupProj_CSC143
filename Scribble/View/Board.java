e7b3988a4e5b97fa4447b105600c7a7dbc5af962
RyanCPeters <petersryan84@gmail.com>
2017-02-15 09:22:22 -0800

    changed the names of the diagrams to make them easier to recognize as diagrams.
    started filling in the basic field and constructor details for window and board in the view package.

--- Scribble/View/Board.java
+++ Scribble/View/Board.java
@@ -1,8 +1,10 @@
 package Scribble.View;
 
+import javax.swing.*;
+
 /** This will extend the content pane or the scene, depending upon what library we decide to use in building the UI.
  * @author Ryan Peters
  * @date 2/12/2017
  */
-public class Board {
+public class Board extends JPanel{
 }
--- Scribble/View/Window.java
+++ Scribble/View/Window.java
@@ -1,9 +1,45 @@
 package Scribble.View;
 
+import javax.swing.*;
+import java.awt.*;
+
 /** A basic window that will extend either a JFrame or an Application class object, depending upon if we decide to use
  * java.swing or javafx.scene libraries.
  * @author Ryan Peters
  * @date 2/12/2017
  */
-public class Window {
+public class Window extends JFrame{
+
+	Board boardPanel;
+	JPanel trayPanel;
+
+	JLabel wordScore;
+	JLabel p1Score;
+	JLabel p2Score;
+	JLabel p3Score;
+	JLabel p4Score;
+	JButton submit;
+	JButton confirm;
+	JButton challenge;
+
+	/**
+	 * Constructs a new frame that is initially invisible.
+	 * <p>
+	 * This constructor sets the component's locale property to the value
+	 * returned by <code>JComponent.getDefaultLocale</code>.
+	 *
+	 * @throws HeadlessException if GraphicsEnvironment.isHeadless()
+	 *                           returns true.
+	 * @see GraphicsEnvironment#isHeadless
+	 * @see Component#setSize
+	 * @see Component#setVisible
+	 * @see JComponent#getDefaultLocale
+	 */
+	public Window() throws HeadlessException {
+		setTitle("Scribble is not Scrabble!!");
+		String playerCount = JOptionPane.showInputDialog
+				("Enter the number of players from 2 to 4: ");
+		int numPlayers = Integer.parseInt(playerCount);
+		boardPanel = new Board();
+	}
 }
