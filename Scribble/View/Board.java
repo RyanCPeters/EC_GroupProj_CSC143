bcf57bba07e24599df67b262a5537242c46e1d8f
RyanCPeters <petersryan84@gmail.com>
2017-03-01 09:53:09 -0800

    fixed some imports and added new subclasses to view and model in order to handle specific duties.

--- Scribble/View/Board.java
+++ Scribble/View/Board.java
@@ -3,6 +3,8 @@ package Scribble.View;
 import javax.swing.*;
 
 /** This will extend the content pane or the scene, depending upon what library we decide to use in building the UI.
+ *  Worth nothing is that this class will also house the board image that we intend to use for our content pane's view.
+ *
  * @author Ryan Peters
  * @date 2/12/2017
  */
--- Scribble/View/Window.java
+++ Scribble/View/Window.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 /** A basic window that will extend either a JFrame or an Application class object, depending upon if we decide to use
- * java.swing or javafx.scene libraries.
+ * java.swing or javafx.scene libraries.3
  * @author Ryan Peters
  * @date 2/12/2017
  */
